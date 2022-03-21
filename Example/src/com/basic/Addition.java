2f6bfdda0fa7bbb582919e73e277a9cdc72fbb03
N Srivalli <N Srivalli@srivalli>
2022-03-21 15:46:01 +0530

    Save Java Program

---
+++ Example/src/com/basic/Addition.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 40;
+		System.out.println("Addition of a and b is: "+a+b);
+
+	}
+
+}
